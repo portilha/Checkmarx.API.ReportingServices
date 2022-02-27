5e9f6adae13bffe0b39c161f0da5bff52264df2d
Pedro Portilha <pedro.portilha@checkmarx.com>
2022-02-27 09:22:26 -0800

    First version

---
+++ Checkmarx.API.ReportingServices/ReportingServiceClient.cs
@@ -0,0 +1,188 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+
+namespace Checkmarx.API.ReportingServices
+{
+    public class ReportingServiceClient
+    {
+        private Uri _acUrl;
+        private Uri _baseURL;
+
+        private string _username;
+        private string _password;
+
+        private readonly HttpClient _httpClient = new HttpClient();
+        private readonly HttpClient _acHttpClient = new HttpClient();
+
+        private DateTime _bearerValidTo;
+
+
+        private AccessControlClient _ac = null;
+        public AccessControlClient AC
+        {
+            get
+            {
+                if (_ac == null && ReportingService != null)
+                {
+                    _ac = new AccessControlClient(_acHttpClient)
+                    {
+                        BaseUrl = _acUrl.AbsoluteUri
+                    };
+                }
+                return _ac;
+            }
+        }
+
+        public ReportingServiceClient(string reportingServerUrl, string acUrl, string username,
+            string password)
+        {
+            if (string.IsNullOrEmpty(reportingServerUrl)) throw new ArgumentNullException(nameof(reportingServerUrl));
+            if (string.IsNullOrEmpty(acUrl)) throw new ArgumentNullException(nameof(acUrl));
+            if (string.IsNullOrEmpty(username)) throw new ArgumentNullException(nameof(username));
+            if (string.IsNullOrEmpty(password)) throw new ArgumentNullException(nameof(password));
+
+            _username = username;
+            _password = password;
+            _acUrl = new Uri(acUrl);
+            _baseURL = new Uri(reportingServerUrl);
+        }
+
+        private ReportingService _reportingService = null;
+
+        public ReportingService ReportingService
+        {
+            get
+            {
+                if (_reportingService == null || (_bearerValidTo - DateTime.UtcNow).TotalMinutes < 5)
+                {
+                    var token = Autenticate(_username, _password);
+                    _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    _acHttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                    _reportingService = new ReportingService(_baseURL.AbsoluteUri, _httpClient);
+
+                    _bearerValidTo = DateTime.UtcNow.AddHours(1);
+                }
+                return _reportingService;
+            }
+        }
+
+        private string Autenticate(string username, string password)
+        {
+            var identityURL = $"{_acUrl}CxRestAPI/auth/identity/connect/token";
+            var kv = new Dictionary<string, string>
+            {
+                { "grant_type", "password" },
+                { "client_id", "reporting_service_api" },
+                { "scope", "reporting_api" },
+                { "username", username },
+                { "password", password }
+            };
+
+
+            var req = new HttpRequestMessage(HttpMethod.Post, identityURL) { Content = new FormUrlEncodedContent(kv) };
+            req.Headers.Add("Accept", "application/json");
+
+            var response = _httpClient.SendAsync(req).Result;
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                JObject accessToken = JsonConvert.DeserializeObject<JObject>(response.Content.ReadAsStringAsync().Result);
+                string authToken = ((JProperty)accessToken.First).Value.ToString();
+                return authToken;
+            }
+            throw new Exception(response.Content.ReadAsStringAsync().Result);
+        }
+
+        public string GetProjectReport(long projectId, string reportName, string format = "pdf")
+        {
+            if (projectId < 0)
+                throw new ArgumentNullException(nameof(projectId));
+
+            if (string.IsNullOrWhiteSpace(reportName))
+                throw new ArgumentNullException(nameof(reportName));
+
+            return getReport(new CreateReportDTO
+            {
+                EntityId = new string[] { projectId.ToString() },
+                OutputFormat = format,
+                ReportName = reportName,
+                TemplateId = 3
+            }, format);
+        }
+
+        public string GetTeamReport(string reportName, string format = "pdf", params string[] teamsFullName)
+        {
+            if (teamsFullName == null || !teamsFullName.Any())
+                throw new ArgumentNullException(nameof(teamsFullName));
+
+            if (string.IsNullOrEmpty(reportName))
+            {
+                if (teamsFullName.Count() == 1)
+                    reportName = teamsFullName.Single().Split("/").Last();
+                else
+                    reportName = "MultipleTeams";
+            }
+
+            return getReport(new CreateReportDTO
+            {
+                EntityId = teamsFullName,
+                OutputFormat = format,
+                ReportName = reportName,
+                TemplateId = teamsFullName.Count() > 1 ? 5 : 4
+            }, format);
+        }
+
+        public string GetScanReport(long scanId, string reportName, string format = "pdf")
+        {
+            if (scanId < 0)
+                throw new ArgumentNullException(nameof(scanId));
+
+            if (string.IsNullOrWhiteSpace(reportName))
+                throw new ArgumentNullException(nameof(reportName));
+
+
+            return getReport(new CreateReportDTO
+            {
+                EntityId = new string[] { scanId.ToString() },
+                OutputFormat = format,
+                ReportName = reportName,
+                TemplateId = 1
+            }, format);
+        }
+
+        private string getReport(CreateReportDTO report, string format)
+        {
+            format = format?.ToLowerInvariant().Trim();
+
+            if (format != "pdf" && format != "json")
+                throw new ArgumentOutOfRangeException("The format can only be json or pdf");
+
+            var createreport = this.ReportingService.CreateReportAsync(report).Result;
+
+
+            while (ReportingService.ReportStatusAsync(createreport.ReportId).Result.ReportStatus == "Processing")
+            {
+                Thread.Sleep(System.TimeSpan.FromSeconds(2));
+            }
+
+            var result = ReportingService.ReportsGETAsync(createreport.ReportId).Result;
+
+            string fileName = report.ReportName + "." + format;
+
+            using (FileStream fs = File.Create(fileName))
+            {
+                result.Stream.CopyTo(fs);
+            }
+
+            return Path.GetFullPath(fileName);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Checkmarx.API.ReportingServices
+Checkmarx.API.ReportingServices
+
+The goal of this project is to provide an API Wrapper for the Checkmarx Reporting Services
+
+Current support version 1.3
+
+
+## Running the Unit Tests
+
+Before running the unit tests please make sure to configure the needed credentials using the [Safe storage of app secrets in development in ASP.NET Core](https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets)
+
+* [Quick access using Visual Studio](https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-5.0&tabs=windows#manage-user-secrets-with-visual-studio-1)
+
